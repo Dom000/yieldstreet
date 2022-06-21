c56e8d8744b358e956f358544931fb0ad6531476
dom000 <godknowsegiboy00@gmail.com>
2022-06-21 22:19:38 +0100

    kmfiw

---
+++ pages/user/deposit.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function deposit() {
+  return (
+    <div>deposit</div>
+  )
+}
+
+export default deposit
---
+++ pages/user/withdrawal.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function withdrawal() {
+  return (
+    <div>withdrawal</div>
+  )
+}
+
+export default withdrawal
